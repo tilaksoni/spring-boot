8e54f51a40ee4adc21a598f78c6caa297fcfc520
Rinku <Rinku@rinku>
2021-07-15 00:22:18 +0530

    #updated

--- IBAN-VALIDATOR/src/main/java/com/iban/IbanValidatorApplication.java
+++ IBAN-VALIDATOR/src/main/java/com/iban/IbanValidatorApplication.java
@@ -1,12 +1,15 @@
 package com.iban;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import javax.annotation.PostConstruct;
 
@@ -31,13 +34,22 @@ public class IbanValidatorApplication {
 	@PostConstruct
     public static void countryIbanLength() {
     	String countryibanlengthString ="",countriesString="";
+    	
     	try {
 			
-    		File resource = new ClassPathResource("countryibanlength.json").getFile();
-    		countryibanlengthString = new String(Files.readAllBytes(resource.toPath()));
-			
-			File countryFile = new ClassPathResource("countries.json").getFile();
-			countriesString = new String(Files.readAllBytes(countryFile.toPath()));
+    		//File resource = new ClassPathResource("countryibanlength.json").getFile();
+    		ClassPathResource resource = new ClassPathResource("countryibanlength.json");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(resource.getInputStream()));
+            countryibanlengthString = reader.lines().collect(Collectors.joining("\n"));
+            reader.close();
+    		//countryibanlengthString = new String(Files.readAllBytes(resource.toPath()));
+            //File countryFile = new ClassPathResource("countries.json").getFile();
+             ClassPathResource resource2 = new ClassPathResource("countries.json");
+			 BufferedReader reader2 = new BufferedReader(new InputStreamReader(resource2.getInputStream()));
+    		 countriesString = reader2.lines().collect(Collectors.joining("\n"));
+             reader2.close();
+             System.out.println(countryibanlengthString);
+			//countriesString = new String(Files.readAllBytes(countryFile.toPath()));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			//e.printStackTrace();
